f4b9f564dd5f6ac613f9269e0e7ba1a20b022f37
unknown <sanhiyo261@gmail.com>
2026-06-05 23:12:00 +0700

    0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ console.log('🔥 CORS BOOTSTRAPPED');
     SwaggerModule.setup('/api/docs', app, document)
   
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
