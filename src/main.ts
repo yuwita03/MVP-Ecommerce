5b40ef845a3e7e1c45ba1fe830fd96c2c150df1c
unknown <sanhiyo261@gmail.com>
2026-06-05 22:18:49 +0700

    fix: corst origin '*'

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ import { DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
+app.setGlobalPrefix('api')
   // Izinkan request dari FE localhost
 app.enableCors({
-  origin: ['http://localhost:5173', 'https://domain-frontend-lo.vercel.app'],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
