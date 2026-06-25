1d7b75cd650f89a48e8b392822e75835cfaf66a6
unknown <sanhiyo261@gmail.com>
2026-06-25 18:29:48 +0700

    fix: migration case sensitive + snapToken + webhook handler

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,5 +1,4 @@
 import { HttpException, Inject, Injectable } from '@nestjs/common';
-// import * as midtransClient from 'midtrans-client'
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { PrismaService } from 'src/common/prisma.service';
 import { Snap } from 'midtrans-client';
@@ -14,101 +13,108 @@ export class PaymentService {
         private prismaService: PrismaService,
         @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger
     ){
-    // Snap adalah produk Midtrans untuk payment page
-    // isProduction: false = sandbox (testing), true = production (uang asli)
         this.snap = new Snap({
-            isProduction: process.env.MIDTRANS_IS_PRODUCTION === 'true', // env cofiguration
-            serverKey: process.env.MIDTRANS_SERVER_KEY!,    // key rahasia, jangan share
-            clientKey: process.env.MIDTRANS_CLIENT_KEY!,    // key untuk frontend
+            isProduction: process.env.MIDTRANS_IS_PRODUCTION === 'true',
+            serverKey: process.env.MIDTRANS_SERVER_KEY!,
+            clientKey: process.env.MIDTRANS_CLIENT_KEY!,
         })
     }
-async createPayment(orderId: string): Promise<{ token: string; redirectUrl: string}>{
-    this.logger.debug(`Creating payment for order: ${orderId}`)
 
-    const order = await this.prismaService.order.findUnique({
-        where: { id: orderId },
-        include: {
-            user: true,
-            items: { include: {product: true }}
-        }
-    });
+    async createPayment(orderId: string): Promise<{ token: string; redirectUrl: string}>{
+        this.logger.debug(`Creating payment for order: ${orderId}`)
 
-    if(!order) throw new HttpException('Order not Found', 404);
-    if (order.status !== 'PENDING') throw new HttpException('Order is not pending', 404);
+        const order = await this.prismaService.order.findUnique({
+            where: { id: orderId },
+            include: {
+                user: true,
+                items: { include: {product: true }}
+            }
+        });
 
-    // Kalau token masih valid, return langsung tanpa bikin transaksi baru
-    if (order.snapToken && order.snapUrl && order.snapTokenExpiredAt) {
-        if (order.snapTokenExpiredAt > new Date()) {
-            return {
-                token: order.snapToken,
-                redirectUrl: order.snapUrl,
+        if(!order) throw new HttpException('Order not Found', 404);
+        if (order.status !== 'PENDING') throw new HttpException('Order is not pending', 404);
+
+        // Kalau token masih valid, return langsung
+        if (order.snapToken && order.snapUrl && order.snapTokenExpiredAt) {
+            if (order.snapTokenExpiredAt > new Date()) {
+                return {
+                    token: order.snapToken,
+                    redirectUrl: order.snapUrl,
+                }
             }
         }
-    }
 
-    const transaction = await this.snap.createTransaction({
-        transaction_details:{
-            order_id: order.id,
-            gross_amount: Number(order.totalPrice)
-        },
-        customer_details:{
-            first_name: order.user.name,
-            email: order.user.email,
-        },
-        item_details: order.items.map(items => ({
-            id: items.productId,
-            name: items.product.name,
-            price: Number(items.price),
-            quantity: items.quantity
-        })),
-    });
+        const transaction = await this.snap.createTransaction({
+            transaction_details:{
+                order_id: order.id,
+                gross_amount: Number(order.totalPrice)
+            },
+            customer_details:{
+                first_name: order.user.name,
+                email: order.user.email,
+            },
+            item_details: order.items.map(items => ({
+                id: items.productId,
+                name: items.product.name,
+                price: Number(items.price),
+                quantity: items.quantity
+            })),
+        });
 
-    // Simpan token ke DB
-    await this.prismaService.order.update({
-        where: { id: orderId },
-        data: {
-            snapToken: transaction.token,
-            snapUrl: transaction.redirect_url,
-            snapTokenExpiredAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
+        // Simpan token ke DB
+        await this.prismaService.order.update({
+            where: { id: orderId },
+            data: {
+                snapToken: transaction.token,
+                snapUrl: transaction.redirect_url,
+                snapTokenExpiredAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
+            }
+        })
+
+        return {
+            token: transaction.token,
+            redirectUrl: transaction.redirect_url
         }
-    })
-
-    return {
-        token: transaction.token,
-        redirectUrl: transaction.redirect_url
     }
-}
 
     async handleWebHook(notifictaion: any): Promise<boolean>{
         this.logger.debug(`Handling webhook: ${JSON.stringify(notifictaion)}`)
 
-        // Webhook = notifikasi otomatis dari Midtrans setelah user bayar
-        // Verifikasi dulu notifikasinya asli dari Midtrans
         const statusResponse = await this.snap.transaction.notification(notifictaion)
 
         const orderId = statusResponse.order_id
-        const transactionStatus = statusResponse.transaction_status; //status dari Midtrans
-        const fraudStatus = statusResponse.fraud_status // deteksi penipuan
+        this.logger.debug(`ORDER ID FROM MIDTRANS: ${orderId}`)
 
-        // ubah tipe
+        const transactionStatus = statusResponse.transaction_status;
+        const fraudStatus = statusResponse.fraud_status
+
         let orderStatus: OrderStatus;
 
-        // capture + accept = pembayaran kartu kredit berhasil dan tidak fraud
         if(transactionStatus === 'capture' && fraudStatus === 'accept'){
             orderStatus = OrderStatus.PAID
         }else if (transactionStatus === 'settlement'){
             orderStatus = OrderStatus.PAID
-        }// cancel/expire = pembayaran dibatalkan atau waktu habis
-        else if (transactionStatus === 'cancel' || transactionStatus === 'expire'){
+        }else if (transactionStatus === 'cancel' || transactionStatus === 'expire'){
             orderStatus = OrderStatus.CANCELLED
         }else{
             orderStatus = OrderStatus.PENDING
         }
-        // kek di db buat update
+
+        // Cek order ada gak sebelum update
+        const order = await this.prismaService.order.findUnique({
+            where: { id: orderId }
+        })
+
+        if (!order) {
+            this.logger.debug(`Order not found: ${orderId}`)
+            return true
+        }
+
         await this.prismaService.order.update({
             where: { id: orderId },
-            data: { status: orderStatus}
+            data: { status: orderStatus }
         })
+
         return true
-}
+    }
 }
